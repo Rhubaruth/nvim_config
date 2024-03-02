9c770800d644037a620560e0ab7ceb9336661cf6
Rhubaruth <kubik.vana@gmail.com>
2024-03-02 19:38:32 +0100

    init

---
+++ rhubaruth/plugins_config/init.lua
@@ -0,0 +1,8 @@
+local path_prefix = 'rhubaruth.plugins_config.' 
+
+require(path_prefix .. 'colorscheme')
+require(path_prefix .. 'whichkey_config')
+require(path_prefix .. 'telescope_config')
+require(path_prefix .. 'treesitter_config')
+require(path_prefix .. 'comment_config')
+require(path_prefix .. 'lsp_config')
