1ed65d05d2906e6f51290da5fa31a2e03408fb92
rhubaruth <kubik.vana@gmail.com>
2024-08-13 08:43:21 +0200

    Delete redundant rhubaruth folder

--- rhubaruth/plugins_config/treesitter_config.lua
+++
@@ -1,9 +0,0 @@
-
-require("nvim-treesitter.configs").setup {
-    ensure_installed = { "c", "lua", "python" },
-    highlight = { enable = true, },
-
-    auto_install = false,
-    
-    ignore_install = { "text", },
-}
