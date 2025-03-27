74e1c0bbe475c4a24c7dfc4658733be90eaa628e
rhubaruth <kubik.vana@gmail.com>
2025-03-27 22:39:06 +0100

    lsp changes

--- lua/setups/lsp.lua
+++ lua/setups/lsp.lua
@@ -1,10 +1,15 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
-end)
+lsp_zero.preset({
+    name = 'none_keymaps',
+    set_lsp_keymaps = false,
+})
+
+-- lsp_zero.on_attach(function(client, bufnr)
+--     -- see :help lsp-zero-keybindings
+--     -- to learn the available actions
+--     lsp_zero.default_keymaps({ buffer = bufnr })
+-- end)
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
@@ -30,8 +35,6 @@ require('lspconfig').r_language_server.setup {
     cmd = { "R", "--slave", "-e", "languageserver::run()" },
     filetypes = { "r", "rmd" },
     -- root_dir = require('lspconfig').util.root_pattern(".git", "DESCRIPTION"),
-    on_attach = function(client, bufnr)
-        -- Your on_attach configurations
-        -- lsp_zero.default_keymaps({ buffer = bufnr })
-    end,
 }
+
+lsp_zero.setup()
